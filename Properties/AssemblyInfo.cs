f3a99f5a43553a28fa0bf9991d34fdd7b7765c12
dragnldy <dragnldy52@gmail.com>
2025-07-22 11:05:00 -0700

    Revert "Change version to 4.9.2.10"
    This reverts commit ec1902ad9c04a16a549bc5e1ac46ba8ec9ae8e12.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("4.0.2.10")]
-[assembly: AssemblyFileVersion("4.0.2.10")] 
+[assembly: AssemblyVersion("4.0.2.9")]
+[assembly: AssemblyFileVersion("4.0.2.9")] 
 
